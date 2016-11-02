44e6f52346055caf6ca7f26b9fce197a68ca681f
yayoung <ega0e@naver.com>
2016-11-02 17:37:09 +0900

    timer error fix
    2초 단위로 다운 되는것을 1초 단위로 다운되도록 수정

---
+++ timer/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace Timer_oo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다.
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마십시오.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("굴림", 20F);
+            this.label1.Location = new System.Drawing.Point(100, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 34);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "3";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("굴림", 20F);
+            this.label2.Location = new System.Drawing.Point(284, 73);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 34);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("굴림", 20F);
+            this.label3.Location = new System.Drawing.Point(199, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(26, 34);
+            this.label3.TabIndex = 2;
+            this.label3.Text = ":";
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(426, 196);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ timer/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Timer_oo
+{
+    public partial class Form1 : Form
+    {
+        private int TimerCount = 0;
+        int i=2;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            TimerCount = 60;
+            timer2.Start();
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+
+            label1.Text = i.ToString();
+            TimerCount -= 1;
+            label2.Text = TimerCount + "";
+            
+            if (TimerCount == 0)
+            {
+
+                TimerCount = 60;
+                label1.Text = i.ToString();
+                i--;
+            }
+
+            if (i == -1)
+            {
+                timer2.Stop();
+                MessageBox.Show("Finish.");
+            }
+        }
+    }
+}
