5f11ae13f6d5b797f9fdc611fc9d1c5a86306775
Jeong Jong In <chongin12@naver.com>
2016-11-05 18:28:42 +0900

    아이템 랜덤 할당 추가(추후 확률 조정 필수!) 블럭 할당 및 아이템 부여 추가 ! 하지만 컴파일 했을 때 아무것도 안보임 !

--- map/map/Form1.cs
+++ map/map/Form1.cs
@@ -18,9 +18,48 @@ namespace map
             InitializeComponent();
 
         }
+        int[] random_setting = new int[20] { 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 3, 3, 3, 3, 3, 4, 5 };
+        public int random_()
+        {
+            Random r = new Random();
+            int num_ = r.Next(0, 19);
+
+
+            return random_setting[num_];
+        }
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            int[,] item_on_block = new int[15, 15];
+            /*
+             * 0 : 아이템 없음
+             * 1 : 추가중
+            */
+            
+            int[,] arr = new int[15, 15] {
+            { 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0 },
+            { 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0 },
+            { 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 0, 1, 2, 1, 1, 2, 2, 2, 1, 1, 2, 1, 0 ,0 },
+            { 0, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 0 },
+            { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1 },
+            { 1, 1, 1, 1, 1, 1, 1, 2, 1, 1, 1, 1, 1, 1, 1 },
+            { 1, 1, 2, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, 1, 1 },
+            { 0, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 0 },
+            { 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 1, 1, 1, 0, 0 },
+            };//맵의 블럭 유무상태 {from 택서소스}
+            /*
+             * 0 : 빈 공간(이미지 : 바닥)
+             * 1 : 부술 수 있는 블럭(이미지 : 걸상)
+             * 2 : 부술 수 없는 블럭(이미지 : 책상)
+             * 3 : 준비중
+            */
+
             PictureBox[,] PictureBox_map = new PictureBox[15, 15];
 
             for (int i = 0; i < 15; ++i)
@@ -36,40 +75,26 @@ namespace map
             PictureBox[,] PictureBox_block = new PictureBox[15, 15];
             for (int i = 0; i < 15; ++i)
             {
-                if (i == 0 || i == 14)
+                for(int j=0; j<15; ++j)
                 {
-                    for (int j = 2; j < 6; ++j)
+                    if (arr[i, j] == 0)
+                    {
+                        continue;
+                    }
+                    else if (arr[i, j] == 1)
                     {
                         PictureBox_block[i, j] = new PictureBox();
                         PictureBox_block[i, j].Location = new Point(j * 50, i * 50);
-                        Random r = new Random();
-                        int num_one_or_two = r.Next(1, 2);
-                        if (num_one_or_two == 1)
-                        {
-                            PictureBox_map[i, j].Image = Properties.Resources.걸상;
-                        }
-                        else
-                        {
-                            PictureBox_map[i, j].Image = Properties.Resources.책상;
-                        }
+                        PictureBox_block[i, j].Image = Properties.Resources.걸상;
                     }
-                    for (int j = 9; j < 14; ++j)
+                    else if (arr[i, j] == 2)
                     {
-                        PictureBox_block[i, j] = new PictureBox(); 
+                        PictureBox_block[i, j] = new PictureBox();
                         PictureBox_block[i, j].Location = new Point(j * 50, i * 50);
-                        Random r = new Random();
-                        int num_one_or_two = r.Next(1, 2);
-                        if (num_one_or_two == 1)
-                        {
-                            PictureBox_map[i, j].Image = Properties.Resources.걸상;
-                        }
-                        else
-                        {
-                            PictureBox_map[i, j].Image = Properties.Resources.책상;
-                        }
+                        PictureBox_block[i, j].Image = Properties.Resources.책상;
                     }
+                    item_on_block[i, j] = random_();
                 }
-
             }
         }
     }
