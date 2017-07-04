c2a0620653fbf84705b948d5f230171b538f4c6f
Kim teak seo <rlaxortj00@gmail.com>
2017-07-04 15:35:01 +0900

    user visible
    user character visible now
    replaced new point to sync mathod
    splash mathod added

--- user/newuser/Form1.cs
+++ user/newuser/Form1.cs
@@ -28,12 +28,13 @@ namespace newuser
         public static PictureBox[,] floooor = new PictureBox[15, 15];
         public static PictureBox[,] splash = new PictureBox[15, 15];
         public static PictureBox[,] blocks = new PictureBox[15, 15];
-        hero myhero = new hero();//hero클래스의 객체로 myhero선언 인자값은 이벤트를 받기위한 form1(this)
         ArrayList list = new ArrayList();
+        hero myhero = new hero();//hero클래스의 객체로 myhero선언 인자값은 이벤트를 받기위한 form1(this)
 
         public Form1()
         {
             InitializeComponent();
+            
         }
 
         private void Form1_KeyDown(object sender, KeyEventArgs e)
@@ -97,6 +98,7 @@ namespace newuser
 
         private void Form1_Load(object sender, EventArgs e)
         {
+            this.Controls.Add(myhero.pb_hero);
             System.Windows.Forms.Timer timer1 = new System.Windows.Forms.Timer();
             inputdata data = new inputdata();//list 배열에 동적으로 inputdata타입인 data를 초기화 후 추가함
             data.key = Keys.Space;//각각의 data는 키값과 상태변수를 가지고있음
@@ -169,14 +171,14 @@ namespace newuser
                     this.Controls.Add(floooor[y, x]);
                 }
 
-            timer1.Interval = 50;
+            timer1.Interval = 60;
             timer1.Start();
             timer1.Tick += new EventHandler(timer1_Tick);
-
         }
 
         private void timer1_Tick(object sender, EventArgs e)//메인 메소드 지속적 리프레쉬
         {
+            
             for (int i = 0; i < list.Count; i++)
             {
                 inputdata temp = list[i] as inputdata;//각각의 키값과 상태를 저장할 temp객체
@@ -189,7 +191,12 @@ namespace newuser
                 {
                     if (temp.stat == 1)
                     {
-                        bubble balloon = new bubble(this, myhero.getuserpos());
+                        if (myhero.i_max_bubble > myhero.i_cur_bubble)
+                        {
+                            myhero.sync(myhero.pb_hero.Location.X, myhero.pb_hero.Location.Y);
+                            bubble balloon = new bubble(this, myhero.getuserpos());
+                            myhero.i_cur_bubble += 1;
+                        }
                     }
                 }
 
@@ -263,85 +270,110 @@ namespace newuser
                 data.stat = 0;
             }
         }
-    }
-    class inputdata
-    {
-        public Keys key { get; set; }
-        public int stat { get; set; }
-    }
-    public class pos
-    {
-        public int x { get; set; }
-        public int y { get; set; }
-    }
-    public class hero
-    {
-        public PictureBox pb_hero;//사용자 캐릭터
-        public int key;
-        pos userpos = new pos();
-        public int level = 2;//상태레벨
-        public int speed = 10;//이동속도
-        public int i_max_bubble = 1;//버블길이
-        public int i_cur_bubble = 1;//버블갯수
-        
-        public hero(Control c_bar)
+        class inputdata
         {
-            userpos.x = 0;
-            userpos.y = 0;
-            i_max_bubble = 2;
-            i_cur_bubble = 0;
-            pb_hero = new PictureBox();
-            pb_hero.Location = new Point(userpos.x, userpos.y);
-            pb_hero.Size = new Size(40, 40);
-            pb_hero.Image = Properties.Resources.F;
-            pb_hero.SizeMode = PictureBoxSizeMode.StretchImage;
-            pb_hero.Visible = true;
-            c_bar.Controls.Add(pb_hero);
+            public Keys key { get; set; }
+            public int stat { get; set; }
         }
-        public void sync(int newx, int newy)
+        public class pos
         {
-            userpos.x = newx;
-            userpos.y = newy;
-            pb_hero.Location = new Point(userpos.x, userpos.y);
+            public int x { get; set; }
+            public int y { get; set; }
         }
-        public pos getuserpos()
-        {
-            return userpos;
-        }
-    }
 
-    public class bubble
-    {
-        public PictureBox bp_bubble = new PictureBox();
-        public PictureBox pb_bubble = new PictureBox();
-        System.Windows.Forms.Timer timer_A;
-        pos bubblepos = new pos();
-        public bubble(Control fuck, pos userpos)
+        public class hero
         {
-            pb_bubble = new PictureBox();
+            public PictureBox pb_hero;//사용자 캐릭터
+            public int key;
+            pos userpos = new pos();
+            public int level = 2;//상태레벨
+            public int speed = 10;//이동속도
+            public int i_max_bubble = 2;//버블길이
+            public int i_cur_bubble = 0;//버블갯수
+            public int getbubble()
+            {
+                return i_cur_bubble;
+            }
+            public void setbubble(int bubblecount)
+            {
+                i_cur_bubble = bubblecount;
+            }
+            public hero()
+            {
+                userpos.x = 0;
+                userpos.y = 0;
+                i_max_bubble = 2;
+                i_cur_bubble = 0;
+                pb_hero = new PictureBox();
+                pb_hero.Location = new Point(userpos.x, userpos.y);
+                pb_hero.Size = new Size(40, 40);
+                pb_hero.Image = Properties.Resources.F;
+                pb_hero.Margin = new System.Windows.Forms.Padding(0);
+                pb_hero.SizeMode = PictureBoxSizeMode.StretchImage;
+                pb_hero.Visible = true;
+            }
+            public void sync(int newx, int newy)
+            {
+                userpos.x = newx;
+                userpos.y = newy;
+                pb_hero.Location = new Point(userpos.x, userpos.y);
+            }
+            public pos getuserpos()
+            {
+                return userpos;
+            }
+        }
 
-            timer_A = new System.Windows.Forms.Timer();
-            timer_A.Interval = 2000;
-            timer_A.Tick += new EventHandler(timer_A_Tick);
-            bubblepos = userpos;
-            pb_bubble.Location = new Point(bubblepos.x, bubblepos.y);
-            pb_bubble.Image = Properties.Resources.bubble;
-            pb_bubble.Size = new System.Drawing.Size(40, 40);
-            pb_bubble.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
-            pb_bubble.Visible = true;
-            fuck.Controls.Add(pb_bubble);
+        public class bubble
+        {
+            public PictureBox pb_bubble = new PictureBox();
+            System.Windows.Forms.Timer timer_A;
+            pos bubblepos = new pos();
+            Control allround = new Control();
             
-            timer_A.Start();
+            public bubble(Control fuck, pos userpos)
+            {
+                timer_A = new System.Windows.Forms.Timer();
+                timer_A.Interval = 2000;
+                timer_A.Tick += new EventHandler(timer_A_Tick);
+                bubblepos.x = userpos.x;
+                bubblepos.y = userpos.y;
+                pb_bubble.Location = new Point(bubblepos.x, bubblepos.y);
+                pb_bubble.Image = Properties.Resources.bubble;
+                pb_bubble.Size = new System.Drawing.Size(40, 40);
+                pb_bubble.Margin = new System.Windows.Forms.Padding(0);
+                pb_bubble.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+                pb_bubble.Visible = true;
+                pb_bubble.BringToFront();
+                fuck.Controls.Add(pb_bubble);
+                allround = fuck;
+                timer_A.Start();
 
-        }
-        public void timer_A_Tick(object sender, EventArgs e)
-        {
-            pb_bubble.Visible = false;
-            //물풍선갯수 감소 (i_cur_bubble
-            //터지는 이펙트ㄱ
-            //터지는것기준으로 양옆 i_max_bubble만큼 픽쳐박스생성
-            //충돌판정필요
-            //종료
+            }
+            public void timer_A_Tick(object sender, EventArgs e)//물풍선 폭발및
+            {
+                pb_bubble.Visible = false;
+                int i, j;
+                for (i = 0; i < 15; i++)
+                {
+                    for (j = 0; j < 15; j++)
+                    {
+
+                    }
+                }
+                //물풍선갯수 감소 (i_cur_bubble
+                //터지는 이펙트ㄱ
+                //터지는것기준으로 양옆 i_max_bubble만큼 픽쳐박스생성
+                //충돌판정필요
+                //종료
+                allround.Controls.Remove(pb_bubble);
+
+            }
+            public void splash()
+            {
+
+            }
         }
     }
+
 }
